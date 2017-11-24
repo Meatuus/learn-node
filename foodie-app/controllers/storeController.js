7f4c81d2c523ee850c696a1b7b3e4ab92be85a94
Tyler <tcameronsmith@gmail.com>
2017-11-25 09:32:10 +1100

    add flash message to create store

--- foodie-app/controllers/storeController.js
+++ foodie-app/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    console.log(req.name);
     res.render('index'); // index is the view we are using
 };
 
@@ -12,9 +11,10 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
     // res.json(req.body);
-    const store = new Store(req.body);
-    await store.save();
-    res.redirect('/');
+    const store = await (new Store(req.body)).save();
+    // await store.save();
+    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
     // store
     //     .save() // fires connection with mongo db
     //     .then(store => {
