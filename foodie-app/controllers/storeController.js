9ace351a9636d24706d884b30a8db2a031097f20
Tyler <tcameronsmith@gmail.com>
2017-11-26 13:07:57 +1100

    add upload photo to store add form

--- foodie-app/controllers/storeController.js
+++ foodie-app/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
+        if (isPhoto) {
+            next(null, true);
+        } else {
+            next({ message: 'That filetype isn\'t allowed!' }, false);
+        }
+    }
+};
 
 exports.homePage = (req, res) => {
     res.render('index'); // index is the view we are using
@@ -9,20 +24,25 @@ exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add storesss'});
 };
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    if (!req.file) {
+        next(); //skip to next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    next();
+}
+
 exports.createStore = async (req, res) => {
-    // res.json(req.body);
     const store = await (new Store(req.body)).save();
-    // await store.save();
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
-    // store
-    //     .save() // fires connection with mongo db
-    //     .then(store => {
-    //         res.render('storeList', )
-    //     })
-    //     .catch(err => {
-    //         throw Error(err);
-    //     })
 };
 
 
@@ -41,6 +61,7 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+    req.body.location.type = 'Point';
     // find and update store
     // redirect to store and tell them it worked
     const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
--- foodie-app/routes/index.js
+++ foodie-app/routes/index.js
@@ -33,8 +33,18 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.getStores)); // call controller to render logic commented out below
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore)); //how to catch errors with async await - wrap function
-router.post('/add/:id', catchErrors(storeController.updateStore));
+
+router.post('/add', 
+  storeController.upload, 
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+); //how to catch errors with async await - wrap function
+router.post('/add/:id', 
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
