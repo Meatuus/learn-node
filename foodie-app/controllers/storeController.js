22170038d8ae89b32ad3f0759e2ff6bc54f1cd7b
Tyler <tcameronsmith@gmail.com>
2017-11-27 15:09:04 +1100

    add tags page, select active tags

--- foodie-app/controllers/storeController.js
+++ foodie-app/controllers/storeController.js
@@ -80,3 +80,9 @@ exports.getStoreBySlug = async (req, res, next) => {
     res.render( 'store', { store, title: store.name })
     if (!store) return next();
 }
+
+exports.getStoresByTag = async (req, res) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag;
+    res.render('tag', { tags, title: 'Tags', tag });
+};
