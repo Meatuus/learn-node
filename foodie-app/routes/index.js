e94db09baba591e43b71d9641b3eced4a6c51a41
Tyler <tcameronsmith@gmail.com>
2017-11-28 11:49:02 +1100

    add is logged in controller to store add route

--- foodie-app/routes/index.js
+++ foodie-app/routes/index.js
@@ -34,7 +34,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores)); // call controller to render logic commented out below
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add', 
   storeController.upload, 
@@ -55,6 +55,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -69,5 +70,7 @@ router.post('/register',
   catchErrors(userController.register),
   authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
