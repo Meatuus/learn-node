be236123f4d5acda4cbe71de5ae180c719514ba3
Tyler <tcameronsmith@gmail.com>
2017-11-29 20:40:00 +1100

    add api to search stores

--- foodie-app/routes/index.js
+++ foodie-app/routes/index.js
@@ -86,4 +86,6 @@ router.post('/account/reset/:token',
     catchErrors(authController.update)
 );
 
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
