64327593a97f3cc2c40d7f8987e51b6bb6d54fc8
Tyler <tcameronsmith@gmail.com>
2017-12-01 13:25:16 +1100

    add review controller and review add post route

--- foodie-app/routes/index.js
+++ foodie-app/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -88,12 +89,18 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage);
 
+router.post('/reviews/:id', 
+    authController.isLoggedIn, 
+    catchErrors(reviewController.addReview)
+);
+
 router.get('/api/search', catchErrors(storeController.searchStores));
 
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+
 
 
 module.exports = router;
