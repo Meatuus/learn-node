ad5a131ce2409c2513a3ac5cec9332b63341a0ab
Tyler <tcameronsmith@gmail.com>
2017-12-01 15:04:53 +1100

    add aggregate to get top stores

--- foodie-app/models/Store.js
+++ foodie-app/models/Store.js
@@ -40,6 +40,9 @@ const storeSchema = new mongoose.Schema({
         ref: 'User',
         required: 'You must supply an author.'
     }
+}, { //below is necessary because when adding object through virtuals it will not appear in teh JSON unless explicitely called. eg calling 'store' will not show reviews data, but calling 'store.reviews' will. adding this fixes this
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
 // Define our indexes
@@ -71,6 +74,53 @@ storeSchema.statics.getTagsList = function () {
         { $group: { _id: '$tags', count: { $sum: 1 } } },
         { $sort: { count: -1 } }
     ]);
+};
+
+storeSchema.statics.getTopStores = function () {
+    return this.aggregate([
+        // Lookup stores and populate their reviews
+        { $lookup: { 
+            from: 'reviews', //this corresponds to 'Review' db. mongoose makes it lowercase and add an 's' on the end
+            localField: '_id', 
+            foreignField: 'store',
+            as: 'reviews' 
+        }},
+        // filter for only items that have 2 or more reviews
+        { $match: {
+            'reviews.1': { $exists: true }
+        }},
+        // add the average reviews field
+        { $addFields: { // this is the same as below using $project -> only available in mongodb v3.4^. instead of creating new fields it just adds to existing
+            averageRating: { $avg: '$reviews.rating' }
+        }},
+        // { $project: {
+        //     photo: '$$ROOT.photo',
+        //     name: '$$ROOT.name',
+        //     reviews: '$$ROOT.reviews',
+        //     averageRating: { $avg: '$reviews.rating' }
+        // }},
+        // sort it by our new field, highest reviews first
+        { $sort: {
+            averageRating: -1
+        }},
+        // limit to at most 10
+        { $limit: 10 }
+    ]);
 }
+
+// find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link
+    localField: '_id', //which field on the store
+    foreignField: 'store' //which field on the reivew
+});
+
+function autopopulate(next) {
+    this.populate('reviews');
+    return next();
+};
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
